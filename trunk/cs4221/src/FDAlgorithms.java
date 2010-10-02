a77df394177fe4d495439dcc571db72bef02d543
swoon09 <swoon09@b32b58cc-e4c6-d7a3-d426-032b388420aa>
2010-10-02 17:36:25 +0000

    updated original code so it can now do step 1 and step 2. added a few helper methods and modified some methods from original version test case for step 1 and 2 is in FDSet.java

--- trunk/cs4221/src/FDAlgorithms.java
+++ trunk/cs4221/src/FDAlgorithms.java
@@ -1,10 +1,5 @@
 import java.util.*;
-/**
- * Write a description of class FD here.
- * 
- * @author (Cui Zheng, please add your name here) 
- * @version (19-9-2010)
- */
+
 public class FDAlgorithms
 {
 
@@ -53,4 +48,134 @@ public class FDAlgorithms
         return right.equals(fd.getRight());
     }
 
+    
+    
+    
+    //check if there are more than 1 attribute on rhs
+    public static boolean isMultipleRHSAttribute(FD fd) {
+    	if (fd.getRight().cardinality()>1)
+        return true;
+    	else return false;
+        }
+        
+    //method to seperate multiple rhs attributes to different fds
+        public static void seperateRHSAttribute(FDSet fdset,FD fd) throws Exception {
+        	int size = fd.getLeft().size();
+        	String left = fd.leftToStringWithDelimiter();
+        	//loop through the attributes on rhs
+        	for (int i = fd.getRight().nextSetBit(0); i >= 0; i = fd.getRight().nextSetBit(i+1)) {
+        		String right="";
+        		char c;
+            	int a=65+i;
+            	c=(char)a;
+            	right+=c;
+                FD fd1 = new FD(left,right,size);
+                //add the single rhs attribute fd to the fd set
+                fdset.addFD(fd1);
+           }
+        }
+        
+        //given a set of fds, this method will return a set of fds with only single rhs attributes
+        public static FDSet allSingleRHSAttribute(FDSet fdset) throws Exception {
+        	FDSet newfdset = new FDSet();
+        	Iterator it = fdset.getFDs().iterator();
+        	while(it.hasNext())  {
+                FD fd=(FD)it.next();
+                if (isMultipleRHSAttribute(fd)) 
+                	seperateRHSAttribute(newfdset, fd);
+                	else
+                		newfdset.addFD(fd); 
+                }
+   return newfdset;
 }
+        
+      //check if there are more than 1 attribute on lhs
+        public static boolean isMultipleLHSAttribute(FD fd) {
+        	if (fd.getLeft().cardinality()>1)
+            return true;
+        	else return false;
+            }
+        
+        
+        //method to remove the extraneous attributes on lhs, an attribute is extraneous in A, lhs of a fd, if 
+        //the attribute is in (A minus attribute) closure on the set of fds. If it is, remove the attribute from lhs
+        //and returns the set of fds with that particular fd having no extraneous attribute
+        public static FDSet seperateLHSAttribute(FDSet fdset,FD fd) throws Exception {
+        	//only for fds with multiple lhs attributes
+        	if (isMultipleLHSAttribute(fd)) {
+        	int size = fd.getLeft().size();
+        	String right = fd.rightToStringWithDelimiter();
+        	String left = "";
+        	//loop through attributes on lhs
+        	for (int i = fd.getLeft().nextSetBit(0); i >= 0; i = fd.getLeft().nextSetBit(i+1)) {
+        		BitSet leftfd = (BitSet)fd.getLeft().clone();
+        		char c;
+            	int a=65+i;
+            	c=(char)a;
+            	BitSet attribute = FD.ToBitSet(c); 
+            	BitSet attribute2 = FD.ToBitSet(c); 
+            	leftfd.xor(attribute);
+            	//find the closure of (A minus attribute) on the fdset
+            	BitSet attriclosure= AttributeClosure(fdset,leftfd);
+            	//check if attribute lies in the closure found above
+            	attribute.and(attriclosure);
+            	//if it does can remove the extraneous attribute from lhs
+            	if (attribute.equals(attribute2)) {
+               		left+=FD.toStringWithDelimiter(leftfd);
+            		 FD fd1 = new FD(left,right,size);
+            		 fdset.removeFD(fd);
+            		 fdset.addFD(fd1);
+            		 //recursive call since fdset is now modified
+            		 seperateLHSAttribute(fdset,fd1);
+            		 break;
+            	}
+           }
+        	}
+        	return fdset;
+        }
+        
+        //given a set of fds, this method will return a set of fds with no extraneous attributes on lhs
+        public static FDSet removeExtraneousAttr(FDSet fdset) throws Exception {
+        	FDSet newfdset=new FDSet(fdset);
+        	Iterator it = fdset.getFDs().iterator();
+        	while(it.hasNext())  {
+                FD fd=(FD)it.next();
+                 newfdset=seperateLHSAttribute(newfdset, fd);
+                }
+   return newfdset;
+}
+        //method to check whether a fd is redundant in a set of fds
+        //a fd, z, is redundant for a set of fds, F, if closure of lhs attributes of z 
+        //on (F minus z) contains the attributes in rhs of z. Returns true or false
+        public static boolean checkForRedundancy(FDSet fdset,FD fd) throws Exception {
+        	FDSet newfdset=new FDSet(fdset);
+        	newfdset.removeFD(fd);
+        	BitSet leftfd = (BitSet)fd.getLeft().clone();
+        	BitSet rightfd = (BitSet)fd.getRight().clone();
+        	BitSet attriclosure= AttributeClosure(newfdset,leftfd);
+        	rightfd.and(attriclosure);
+        	if (rightfd.equals(fd.getRight()))
+        	return true;
+        	else return false;
+        }
+        
+        //given a set of fds, return a set where there is no redundant attributes in the set
+        public static FDSet removeRedundantFDs(FDSet fdset) throws Exception {
+        	FDSet newfdset=new FDSet(fdset);
+        	FDSet newfdset2=new FDSet(fdset);
+        	Iterator it = fdset.getFDs().iterator();
+        	while(it.hasNext())  {
+                FD fd=(FD)it.next();
+                 if (checkForRedundancy(newfdset,fd))
+                	 newfdset2.removeFD(fd); 
+                }
+        	return newfdset2;
+        }
+       
+ 
+}
+
+
+
+
+
