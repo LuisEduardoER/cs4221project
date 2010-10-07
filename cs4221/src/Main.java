5fa780f6650ffce935f0bea07afcca25709207e4
hoongkwan <hoongkwan@nus.edu.sg>
2010-10-07 15:01:18 +0000

    a few syntax updates

--- cs4221/src/Main.java
+++ cs4221/src/Main.java
@@ -5,28 +5,32 @@ public class Main {
      */
     public static void main(String[] args) throws Exception{
 
-        Control control = new Control();
         int maxFD = 10, width = 2;
 
         FDSet fdset;
         BigSet bigset;
 
-        ///Step 0 : Read the input and put it into a FDSet namely fdset
+        ///Step 0 : Read the input and put it into a FDSet::fdset
         String input = "";
         for (int i = 0; i < args.length; i++){
             input = input.concat(args[i]);
         }
         System.out.println(input);
+
+        ReadInput readInput = new ReadInput();
         String[][]fds = new String[maxFD][width];
-        fds = control.readInput(input, maxFD, width);
+        fds = readInput.readInput(input, maxFD, width);
+
         fdset = new FDSet(fds,maxFD);
         System.out.println();
+        System.out.println("This is the original FDSet with the FDs");
         fdset.printFDSet();
 
-        ///Step 3 Partition : Convert 1 FDSet into a few FDSets(BigSet)
+        ///Step 3 Partition : Convert the FDSet into a few FDSets(BigSet)
         Partition p = new Partition();
         bigset = p.works(fdset);
         System.out.println();
+        System.out.println("This is the BigSet(result (p/s:work in progress))");
         bigset.printBigSet();
 
         ///Step 5 Transistivity
