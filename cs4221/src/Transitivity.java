21b4f1b2b3d1e33166af856b155e230c2e00747b
guozheng86 <guozheng86@b32b58cc-e4c6-d7a3-d426-032b388420aa>
2010-10-11 18:14:11 +0000

    i change m6_displayRelation and add m6_grouping.. m6 no longer requires m3 and m4. However, m5 still requires m4. M4 seems not working properly.

--- cs4221/src/Transitivity.java
+++ cs4221/src/Transitivity.java
@@ -53,7 +53,7 @@ public class Transitivity {
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input.length; j++) {
                 boolean flag = false;
-                if (input[i][j]!=null) {
+                if (input[i][j] != null) {
                     //check if there is transitivity for this FD
                     flag = m5_checkTransitivity(input, i, j);
                 }
@@ -72,11 +72,11 @@ public class Transitivity {
         for (int i = 0; i < input.length; i++) {
             String[] st;
             String LHS = ".", RHS = ".";
-            if (input[x][i]!=null) {
+            if (input[x][i] != null) {
                 st = input[x][i].split("->");
-                RHS = st[1]; 
+                RHS = st[1];
                 for (int j = 0; j < input.length; j++) {
-                    if (input[j][y]!=null) {
+                    if (input[j][y] != null) {
                         st = input[j][y].split("->");
                         LHS = st[0];
                     }
@@ -89,14 +89,15 @@ public class Transitivity {
         }
         return false;
     }
-    public String[][] m5_eliminateTransitivity(String[][] input,int x,int y){
+
+    public String[][] m5_eliminateTransitivity(String[][] input, int x, int y) {
         //Trim the FDset to remove transitivity FD
         String[] temp = input[x][y].split("->");
-        String newFdSet[][] = new String[fdset.length-1][fdset.length-1];
+        String newFdSet[][] = new String[fdset.length - 1][fdset.length - 1];
         String LHS = temp[0], RHS = temp[1];
         int cnt = 0;
-        for(int i=0; i<fdset.length; i++){
-            if(!fdset[i][0].equals(LHS) || !fdset[i][1].equals(RHS)){
+        for (int i = 0; i < fdset.length; i++) {
+            if (!fdset[i][0].equals(LHS) || !fdset[i][1].equals(RHS)) {
                 newFdSet[cnt][0] = fdset[i][0];
                 newFdSet[cnt][1] = fdset[i][1];
                 cnt++;
@@ -104,41 +105,69 @@ public class Transitivity {
         }
         return newFdSet;
     }
-    public void m5_printFDset(){
+
+    public void m5_printFDset() {
         //Aim: To print out the FDset
-        for(int i=0; i<fdset.length; i++){
+        for (int i = 0; i < fdset.length; i++) {
             System.out.print(fdset[i][0] + " -> ");
             System.out.println(fdset[i][1]);
         }
     }
-    public void m5_printFDBigset() throws Exception{
+
+    public void m5_printFDBigset() throws Exception {
         //Aim: To print out the FDBigset
         BigSet bigset;
-        FDSet f = new FDSet(fdset,fdset.length);
+        FDSet f = new FDSet(fdset, fdset.length);
         Partition p = new Partition();
         bigset = p.works(f);
         Merge m = new Merge();
-        bigset = m.works(bigset,f);
+        bigset = m.works(bigset, f);
         bigset.printBigSet();
     }
-    public void m6_displayRelation() throws Exception{
-        // uses method 3
+
+    public void m6_displayRelation() throws Exception {
+        // 
         LinkedList ll = new LinkedList();
         BigSet bigset;
-        FDSet f = new FDSet(fdset,fdset.length);
-        Partition p = new Partition();
-        bigset = p.works(f);
-        bigset.printBigSet();
+        bigset = m6_grouping();
         ll = bigset.BigSetToRelation();
-        for(int i=0; i<ll.size(); i++){
+        for (int i = 0; i < ll.size(); i++) {
             System.out.println(ll.get(i));
         }
         System.out.println();
     }
+
+    private BigSet m6_grouping() throws Exception {
+        BigSet bs = new BigSet();
+        String[][] tempset = new String[fdset.length][2];
+        String[][] TDset = fdset;
+        int k;
+        for (int i = 0; i < TDset.length; i++) {
+            if (!TDset[i][0].equals(".")) {
+                k = 0;
+                tempset[k][0] = TDset[i][0];
+                tempset[k][1] = TDset[i][1];
+                for (int j = i+1; j < TDset.length; j++) {
+                    if (TDset[i][0].equals(TDset[j][0]) ||
+                            (TDset[i][0].equals(TDset[j][1]) && TDset[i][1].equals(TDset[j][0]))) {
+                        k++;
+                        tempset[k][0] = TDset[j][0];
+                        tempset[k][1] = TDset[j][1];
+                        TDset[j][0] = ".";
+                        TDset[j][1] = ".";
+                    }
+                }
+                FDSet f = new FDSet(tempset, k + 1);
+                bs.addFDSet(f);
+            }
+        }
+        return bs;
+    }
+
     public void test() throws Exception {
         //Aim: 3 set of transitivity test cases
         System.out.println("TestRelation1 = {A -> B, A -> C, B -> C, C -> D, A -> D}");
-        String testdata1[][] = {{"A", "B"}, {"A", "C"}, {"B", "C"}, {"C","D"},{"A","D"}};
+        String testdata1[][] = {{"A", "B"}, {"A", "C"}, {"B", "C"}, {"C", "D"}, {"A", "D"}};
 
         fdset = testdata1;
         testdata1 = m5_plotTransitivity();
@@ -149,7 +178,7 @@ public class Transitivity {
         System.out.println("TestRelation2 = {OP -> CD, CD -> OP, " +
                 "OP -> A, AO -> B, BP -> C , C -> A}");
         String testdata2[][] = {{"O,P", "C,D"}, {"C,D", "O,P"},
-                  {"O,P", "A"},{"A,O","B"},{"B,P","C"},{"C","A"}};
+            {"O,P", "A"}, {"A,O", "B"}, {"B,P", "C"}, {"C", "A"}};
         fdset = testdata2;
         testdata2 = m5_plotTransitivity();
         m5_indentifyTransitivity(testdata2);
@@ -158,12 +187,11 @@ public class Transitivity {
 
         System.out.println("TestRelation3 = {B -> D, D -> B, A -> B, B -> C, A -> F}");
         String testdata3[][] = {{"B", "D"}, {"D", "B"},
-                  {"A", "B"},{"B","C"},{"A","F"}};
+            {"A", "B"}, {"B", "C"}, {"A", "F"}};
         fdset = testdata3;
         testdata3 = m5_plotTransitivity();
         m5_indentifyTransitivity(testdata3);
         m5_printFDBigset();
         m6_displayRelation();
     }
-
 }
